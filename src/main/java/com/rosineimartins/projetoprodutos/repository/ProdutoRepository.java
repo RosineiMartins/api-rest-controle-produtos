acb0dea7549a210386b2473fb66465406e65b4d6
RosineiMartins <rosineimm@gmail.com>
2023-05-07 21:37:12 -0300

    First commit

---
+++ src/main/java/com/rosineimartins/projetoprodutos/repository/ProdutoRepository.java
@@ -0,0 +1,78 @@
+package com.rosineimartins.projetoprodutos.repository;
+
+
+import com.rosineimartins.projetoprodutos.model.Produto;
+
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Optional;
+
+public class ProdutoRepository {
+
+    List<Produto> produtoList = new ArrayList<>();
+    private Integer ultimoId = 0;
+
+    /**
+     * Metodo que retorna uma lista de produtos
+     *
+     * @return lista de produto
+     */
+    private List<Produto> obterTodos() {
+        return produtoList;
+    }
+
+    /**
+     * Metodo para cadastro de produtos
+     *
+     * @param produto
+     * @return produto
+     */
+    private Produto cadastrarProduto(Produto produto) {
+        ultimoId++;
+        produto.setId(ultimoId);
+        produtoList.add(produto);
+        return produto;
+    }
+
+    /**
+     * Metodo que retorna o produto encontrado pelo su Id
+     *
+     * @param id do produto que será localizado
+     * @return retorna um produto caso seja encontrado
+     */
+    private Optional<Produto> obterPorId(Integer id) {
+        return produtoList.stream()
+                .filter(produto -> produto.getId() == id)
+                .findFirst();
+    }
+
+    /**
+     * Metodo que deleta o produto por id
+     *
+     * @param id do produto a ser deletado
+     */
+    private void deletarProduto(Integer id) {
+        produtoList.removeIf(produto -> produto.getId() == id);
+
+    }
+
+    /**
+     * Metodo para atualizar na lista
+     * @param produto que será aualizado
+     * @return o produto após atualizar
+     */
+    private Produto atualizar(Produto produto) {
+        //1o encontrar o produto na lista
+        Optional<Produto> produtoEncontrado = obterPorId(produto.getId());
+        if (produtoEncontrado.isEmpty()) {
+            throw new InputMismatchException("Produto nao encontrado");
+        }
+        //2o remover o poduto da lista
+        deletarProduto(produto.getId());
+        //3o adiciona produto
+        produtoList.add(produto);
+        return produto;
+    }
+
+}
---
+++ src/test/java/com/rosineimartins/projetoprodutos/ProjetoProdutosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rosineimartins.projetoprodutos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoProdutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
